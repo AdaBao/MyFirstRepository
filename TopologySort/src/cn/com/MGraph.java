6818910740134799a4105e004f2c4a177958c89a
AdaBao <33488704+AdaBao@users.noreply.github.com>
2018-09-30 21:24:39 +0800

    拓扑排序

---
+++ TopologySort/src/cn/com/MGraph.java
@@ -0,0 +1,55 @@
+package cn.com;
+
+import java.util.Scanner;
+
+public class MGraph {
+	private int vertexNum;
+	private String[] vertexs;
+	private int [][]arc;
+	private int []in;//表示入度,这里需要假设第一个节点的入度为0
+	
+	public void init(){
+		System.out.println("输入节点数:");
+		Scanner scanner=new Scanner(System.in);
+		this.vertexNum=Integer.parseInt(scanner.nextLine());
+		System.out.println("输入各个节点的值:");
+		String valueStr=scanner.nextLine();
+		this.vertexs=valueStr.split(" ");
+		this.arc=new int[this.vertexNum][this.vertexNum];
+		in=new int[this.vertexNum];
+		System.out.println("输入邻接矩阵:");
+		for(int i=0; i<this.vertexNum; i++)
+			for(int j=0; j<this.vertexNum; j++){
+				this.arc[i][j]=scanner.nextInt();
+				if(this.arc[i][j]==1)
+					in[j]=in[j]+1;
+			}
+	}
+	
+	public void topologySort(){
+		int next=0;
+		while(true){
+			next=-1;
+			for(int i=0; i<this.vertexNum; i++){
+				if(in[i]==0)
+				{
+					next=i;
+					in[i]=-1;
+					
+					for(int j=0; j<this.vertexNum; j++){
+						if(this.arc[i][j]>=0)
+							in[j]--;
+					}
+					
+					break;
+				}
+			
+			
+			}
+			
+			if(next==-1)
+				break;
+			System.out.println(this.vertexs[next]);
+		}
+}
+	}
